0a000165f8d29b4984c8cc86b955dae12a452109
Thanh Doan <dtdoa5@student.monash.edu>
2017-05-23 00:21:44 +1000

    Add comments of MorseDecoder.js

--- Application/Morse/MorseDecoder.js
+++ Application/Morse/MorseDecoder.js
@@ -9,7 +9,12 @@ class MorseDecoder {
 	constructor() {
 		this.morseCode = "";
 	}
-
+	/**
+	* addSignal
+	*@return decodedLetter if a letter gap symbol is read
+	*@return decodedLetter + " " if a word gap symbol is read
+	*@param
+	*/
 	addSignal(morseSignal) {
 		if (morseSignal == morse.letterGapSym) {
 			let decodedLetter = morse.table[this.morseCode];
