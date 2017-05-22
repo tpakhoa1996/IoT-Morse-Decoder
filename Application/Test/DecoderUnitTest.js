cf2b92f14258fb505739624c4a83ae266f934918
Thanh Doan <dtdoa5@student.monash.edu>
2017-05-22 19:42:45 +1000

    Add a unit test method for Decoder

---
+++ Application/Test/DecoderUnitTest.js
@@ -0,0 +1,20 @@
+var chai = require('chai');
+var expect = chai.expect;
+var MorseDecoder = require("./Morse/MorseDecoder.js");
+
+var decoder = new MorseDecoder();
+
+describe('Decoder', function() {
+	var testSignalString = "SL_LSS&LLL&LLS_";
+	var result = "";
+	it('MorseDecoder should be able to decode the signal into English words',function() {
+		for (var i=0; i < testSignalString.length; i++) {
+			var character = decoder.addSignal(testSignalString[i]);
+			if (character != null) {
+				result += character;
+			}
+		}
+		
+	expect(result).to.equal("A DOG ");
+	});
+});
