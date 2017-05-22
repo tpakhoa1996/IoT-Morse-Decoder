51cf368a2a063618efd4fc9e6d3d420309f0ca37
Brian <khoaphananhtran@gmail.com>
2017-05-23 05:15:19 +1000

    Add SK signals and tranmission controller

--- Application/Signal/SignalController.js
+++ Application/Signal/SignalController.js
@@ -1,7 +1,11 @@
 /*
  * This module will read in the motion signal from the sensor and send it
  * to MotionTranslator to get back the morse signal. Then the morse signal
- * will be emitted out
+ * will be emitted out.
+ *
+ * This module also help client control tranmission process and detect 
+ * when we meet a SK signal
+ *
  * @author: Khoa (Brian) Tran - patra3@student.monash.edu
  */
 
@@ -14,7 +18,9 @@ let board = new five.Board(),
 	signalEmitter = new Events(),
 	logger = new Winston.Logger(),
 	motionTranslator = new MotionTranslator(),
-	Signal = require("../models/Signal.model.js");
+	Signal = require("../models/Signal.model.js"),
+	sensorControlRef = require("../models/database.js").ref("SensorControl"),
+	morse = require("../config.morse.js");
 
 // Configure logger
 logger.configure({
@@ -47,45 +53,137 @@ let processNewSignal = (signal) => {
 	}
 };
 
+
+// Initiate the SK signal
+let skSignal = ["dot", "dot", "dot", "dash", "dot", "dash"];
+
+
+// This variable is used to store the last six morse signal in order to
+// check whether we reach a SK signal
+let lastMorseSignals = "";
+
+// Listen to when a new morse signal is sent out
+signalEmitter.on("data", (morseSignal) => {
+
+	// Update lastMorseSignals
+	lastMorseSignals += morseSignal.signal;
+	if (lastMorseSignals.length > 6)
+		lastMorseSignals = lastMorseSignals.slice(1);
+
+
+	// Test whether we have received 6 morse signals to be able 
+	// receive a SK signal
+	if (lastMorseSignals.length == 6) {
+
+		// Check whether or not we reached a SK signal
+		let isSkSignal = true;
+		skSignal.forEach( (signal, index) => {
+			signal = morse[signal + "Sym"];
+			if (lastMorseSignals[index] != signal) {
+				isSkSignal = false;
+				return false;
+			}
+		});
+
+		if (isSkSignal) {
+			// If a SK signal is received, set sensor to off
+			sensorControlRef.update({
+				"value": "off"
+			});
+			let signal = new Signal("morse", "SK, End of transmission", new Date());
+
+			// Upload the signal to firebase
+			signal.save();
+		}
+	}
+});
+
 // Listen to when the board is ready
 board.on("ready", () => {
 	let motion = new five.Motion(7);
+	let lastMotionSignal = null;
+
+	// Listen to when the sensor is toggled
+	sensorControlRef.on("value", (snap) => {
+
+		// Check whether the sensor is set to on or off or undefined
+		data = snap.val();
+		if (data == null || (data.value != "on" && data.value != "off")) {
+			// Sensor status is undefined => Set status to off
+			sensorControlRef.update({
+				value: "off"
+			});
+			return;
+		} else if (data.value == "off") {
+			// Sensor is set to off => execute procedure to
+			// stop transmission
+			// Emit "motionend" if there is an ongoing motion
+			if (lastMotionSignal == "motionstart")
+				motion.emit("motionend");
+			// Remove all listeners of event motion so that
+			// notthing can hear heard from motion sensor
+			motion.removeAllListeners();
+			// reset SK signal checking process
+			lastMorseSignals = "";
+			return;
+		}
+
+		// Throw away the old emitter
+		motion = new five.Motion(7);
+		// Create a new translator
+		motionTranslator = new MotionTranslator();
+		// This variable is used to prevent the situation when a motionend
+		// is heard before any motionstart
+		lastMotionSignal = null;
 
 
-	// Listen to the event emmited when the motion sensor is ready to work
-	// This function will send this signal to firebase
-	motion.on("calibrated", () => {
-		logger.log("info", "The sensor calibrated at", new Date().toLocaleString());
+		// Listen to the event emmited when the motion sensor 
+		// is ready to work
+		// This function will send this signal to firebase
+		motion.on("calibrated", () => {
+			logger.log("info", "The sensor calibrated at", new Date().toLocaleString());
 
-		let signal = new Signal("motion", "calibrated", new Date());
-		signal.save();
+			let signal = new Signal("motion", "calibrated", new Date());
+			signal.save();
 
-	});
-
-
-	// Listen to the event when a motion is detected by motion sensor
-	// This function will send this signal to firebase and MotionTranslator
-	motion.on("motionstart", () => {
-		logger.log("info", "Motion started at", new Date().toLocaleString());
-
-		let signal = new Signal("motion", "motionstart", new Date());
-		signal.save();
-
-		processNewSignal(signal.getObject());
-	});
+		});
 
 
-	// Listen to the event when a motion has stopped
-	// This function will send this signal to firebase and MotionTranslator
-	motion.on("motionend", () => {
-		logger.log("info", "Motion ended at", new Date().toLocaleString());
+		// Listen to the event when a motion is detected by motion sensor
+		// This function will send this signal to firebase and
+		// MotionTranslator
+		motion.on("motionstart", () => {
+			lastMotionSignal = "motionstart";
 
-		let signal = new Signal("motion", "motionend", new Date());
-		signal.save();
+			logger.log("info", "Motion started at", new Date().toLocaleString());
 
-		processNewSignal(signal.getObject());
+			let signal = new Signal("motion", "motionstart", new Date());
+			signal.save();
+
+			processNewSignal(signal.getObject());
+		});
+
+
+		// Listen to the event when a motion has stopped
+		// This function will send this signal to firebase and
+		// MotionTranslator
+		motion.on("motionend", () => {
+			if (lastMotionSignal != "motionstart")
+				return;
+
+			lastMotionSignal = "motionend";
+
+			logger.log("info", "Motion ended at", new Date().toLocaleString());
+
+			let signal = new Signal("motion", "motionend", new Date());
+			signal.save();
+
+			processNewSignal(signal.getObject());
+		});
 	});
 });
+
+
 
 
 // Return the event emitter
--- Application/public/main.js
+++ Application/public/main.js
@@ -8,7 +8,8 @@ $(document).ready( function() {
 
 	var db = firebase.database(),
 		signalRef = db.ref("Signal"),
-		messageRef = db.ref("Message");
+		messageRef = db.ref("Message"),
+		sensorControlRef = db.ref("SensorControl");
 
 	var signal = $("#signal"),
 		message = $("#message");
@@ -17,7 +18,7 @@ $(document).ready( function() {
 		var data = snap.val();
 		data.date = new Date(data.date);
 		
-		if (data.type == "morse")
+		if (data.type == "morse" && morse_code[data.signal])
 			data.signal = morse_code[data.signal];
 
 		var output_string = "At " + data.date.toLocaleString() + ", there is a " + data.type + " signal received: " + data.signal;
@@ -35,14 +36,63 @@ $(document).ready( function() {
 
 	messageRef.on("child_added", function(snap) {
 		var data = snap.val();
-		console.log("Message get: " + data.string);
 
-		var first_element = message.children().first();
+		var first_element = message.children();
 		
-		if (first_element.prop("tagName").toLowerCase() == "strong")  {
-			first_element.remove();
-			message.text(data.string);
+		if (first_element.length > 0) {
+			first_element = first_element.first();
+			if (first_element.prop("tagName").toLowerCase() == "strong")  {
+				first_element.remove();
+				message.text(data.string);
+			}
 		} else 
 			message.text( message.text() + data.string);
 	});
+
+	var onBtn = $("#on-btn"),
+		offBtn = $("#off-btn"),
+		loadingBtn = $("#loading-btn");
+
+	function showOnBtn() {
+		offBtn.hide();
+		loadingBtn.hide();
+		onBtn.show();
+	}
+
+	function showOffBtn() {
+		onBtn.hide();
+		loadingBtn.hide();
+		offBtn.show();
+	}
+
+	function showLoadingBtn() {
+		onBtn.hide();
+		offBtn.hide();
+		loadingBtn.show();
+	}
+
+	sensorControlRef.on("value", function(snap) {
+		var data = snap.val();
+
+		if (data == null)
+			showLoadingBtn();
+		else if (data.value == "on")
+			showOnBtn();
+		else if (data.value == "off")
+			showOffBtn();
+		else {
+			sensorControlRef.update({ "value": "off" });
+		}
+	});
+
+	onBtn.on("click", function() {
+		showLoadingBtn();
+		sensorControlRef.update({ "value": "off" });
+	});
+
+	offBtn.on("click", function() {
+		showLoadingBtn();
+		sensorControlRef.update({ "value": "on" });
+	});
+
 });
